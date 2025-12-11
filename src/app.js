87ca38c8fdcda3fb26d6f3b78a88a88134a7bf9c
Aman Mukhiya <amanmukhiya4@gmail.com>
2025-12-11 16:14:06 +0545

    First Commit for Folder creation

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+//routes
+
+export default app;
