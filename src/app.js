1f519abdd9e38870644e1bc12bd7cb6aeda870fe
Aman Mukhiya <amanmukhiya4@gmail.com>
2025-12-15 21:02:10 +0545

    Implement employee and admin models with password hashing and JWT token generation; add API error and response utilities; enhance app configuration

--- src/app.js
+++ src/app.js
@@ -6,7 +6,10 @@ const app = express();
 
 app.use(cors());
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({limit: "16kb"}));// encodeds url space -> %20
+app.use(express.static("public"))// if i took any files this is where i store
+
 
 //routes
 
--- src/models/employee.model.js
+++ src/models/employee.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import  jwt  from "jsonwebtoken";
 
 const employeeSchema = new mongoose.Schema(
   {
@@ -29,5 +31,33 @@ const employeeSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+employeeSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+employeeSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+employeeSchema.methods.generateAccessToken = async function(){
+  return jwt.sign({
+    _id: this.id,
+    name: this.name,
+    role: this.role,
+    phone: this.phone
+  },process.env.ACCESS_TOKEN_SCERET,{
+    expiresIn: "1d"
+  });
+}
+
+employeeSchema.methods.generateRefreshToken = async function(){
+  return jwt.sign({
+    _id: this.id,
+  },process.env.REFRESH_TOKEN_SCERET,{
+    expiresIn: "7d"
+  });
+}
 
 export const Employee = mongoose.model("Employee", employeeSchema);
